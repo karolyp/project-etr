854a62cdb8d8526c739e85cad07d863016367f56
Pákozdi Károly <pakxadt@gmail.com>
2017-04-13 18:37:45 +0200

    Added everything

---
+++ src/main/java/hu/adatb/jetr/exception/WrongUsernamePasswordException.java
@@ -0,0 +1,12 @@
+package hu.adatb.jetr.exception;
+
+public class WrongUsernamePasswordException extends Exception {
+
+	private static final long serialVersionUID = 7228907984622252108L;
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+}
